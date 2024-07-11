ba5d0f58f940c70b2e3bf80b4eb30cfe27fa6184
Yash-6503 <yashwalke6503@gmail.com>
2024-07-11 18:56:47 +0530

    Prime Numbers

--- Exercises/Q4.c
+++ Exercises/Q4.c
@@ -1,18 +1,32 @@
 // Write a C Program to Print Prime Numbers From 1 to N
 
-#include<stdio.h>
+#include <stdio.h>
+
+int isPrime(int num)
+{
+    if (num <= 1)
+        return 0; // 0 and 1 are not prime numbers
+    for (int i = 2; i * i <= num; i++)
+    {
+        if (num % i == 0)
+            return 0; // If divisible by any number other than 1 and itself
+    }
+    return 1; // Number is prime
+}
 
 int main()
 {
-    int prime, n, i;
+    int n;
     printf("Enter upto which n number you wants to print the prime number: ");
-    scanf("%d", &prime);
+    scanf("%d", &n);
 
-    for(i=1; i<=prime; i++)
+    printf("Prime numbers from 1 to %d are: ", n);
+    for (int i = 1; i <= n; i++)
     {
-        if(i %2 == 1)
+        if (isPrime(i))
         {
             printf("%d ", i);
         }
     }
+    return 0;
 }
