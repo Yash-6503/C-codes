150ee54c107b22ce816727306a14d98db59d206f
Yash-6503 <yashwalke6503@gmail.com>
2024-07-11 18:18:34 +0530

    Multiplication of Floating numbers

---
+++ Exercises/Q1.c
@@ -0,0 +1,12 @@
+// C Program to Multiply two Floating - Point Numbers
+
+#include <stdio.h>
+int main()
+{
+    float num1, num2, result;
+    printf("\nEnter 2 Numbers: ");
+    scanf("%f%f", &num1, &num2);
+    result = num1 * num2;
+    printf("\nMultiplication of two Floating - Point Numbers is %f ", result);
+    return 0;
+}
