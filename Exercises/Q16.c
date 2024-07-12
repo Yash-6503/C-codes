f99602f0d014b32a041d8d7e13956e7d82df0734
Yash Walke <142409974+Yash-6503@users.noreply.github.com>
2024-07-12 12:45:09 +0000

    Reverse Pyramid Pattern

---
+++ Exercises/Q16.c
@@ -0,0 +1,16 @@
+// C Program to Print Simple Reverse Pyramid Pattern 
+
+#include<stdio.h>
+int main()
+{
+    int i, j;
+    for(i=0; i<=4; i++)
+    {
+        for(j=4; j >= i; j--)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
