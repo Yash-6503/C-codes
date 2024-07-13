1f790a6aaec472e17c74b2fa7a0aa4fb939c27a8
Yash Walke <142409974+Yash-6503@users.noreply.github.com>
2024-07-13 06:20:07 +0000

    2Dimensional Array

--- Arrays/2DArray.c
+++ Arrays/2DArray.c
@@ -1,1 +1,16 @@
 // Write a C Program to Print 2D Array
+
+#include<stdio.h>
+int main()
+{
+    int arr[2][3] = {{1, 2, 3}, {3, 2, 1}};
+    for(int i = 0; i < 2; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
