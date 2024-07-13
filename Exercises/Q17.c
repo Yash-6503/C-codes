d1d9b40d970fa4efe6dbb5a54cde4c89fd98b209
Yash Walke <142409974+Yash-6503@users.noreply.github.com>
2024-07-13 05:07:41 +0000

    Numbers Pyramid

---
+++ Exercises/Q17.c
@@ -0,0 +1,25 @@
+//Write a C Program to Print the numbers pyramid
+
+#include<stdio.h>
+
+int main()
+{
+    int i, j;
+    for(i = 1; i <= 5; i++)
+    {
+        for(j = 1; j <= i; j++)
+        {
+            printf("%d ", i);
+        }
+        printf("\n");
+    }
+
+    for(i = 1; i <= 5; i++)
+    {
+        for(j = 5; j >= i; j--)
+        {
+            printf("%d ", i);
+        }
+        printf("\n");
+    }
+}
