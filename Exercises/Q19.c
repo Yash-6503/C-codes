2584a90f92d421c0b8f7e215ffa413e7a219fc8c
Yash Walke <142409974+Yash-6503@users.noreply.github.com>
2024-07-13 05:28:29 +0000

    All Pyramid Patterns

---
+++ Exercises/Q19.c
@@ -0,0 +1,69 @@
+//Practice of all Pyramid examples
+
+#include<stdio.h>
+int main()
+{
+    int i, j;
+    printf("\nSquare Pattern\n");
+    for(i = 1; i <= 5; i++)
+    {
+        for(j = 1; j <= 5; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    printf("\nTriangle Pattern\n");
+    for(i = 1; i <= 5; i++)
+    {
+        for(j = 1; j <= i; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    
+    printf("\nReverse Triangle Pattern\n");
+    for(i = 1; i <= 5; i++)
+    {
+        for(j = 5; j >= i; j--)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    printf("\nNumbers Triangle Pattern\n");
+    for(i = 1; i <= 5; i++)
+    {
+        for(j = 1; j <= i; j++)
+        {
+            printf("%d ", i);
+        }
+        printf("\n");
+    }
+
+    
+    printf("\nReverse Numbers Pattern\n");
+    for(i = 1; i <= 5; i++)
+    {
+        for(j = 5; j >= i; j--)
+        {
+            printf("%d ",i);
+        }
+        printf("\n");
+    }
+
+    
+    printf("\nAlphabets Triangle Pattern\n");
+    for(char a = 'A'; a <= 'E'; a++)
+    {
+        for(char b = 'A'; b <= a; b++)
+        {
+            printf("%c ",a);
+        }
+        printf("\n");
+    }
+    return 0;
+}
