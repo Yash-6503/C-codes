691a146c078ef4ae384daec72ee42dde7f1aaea4
Yash Walke <142409974+Yash-6503@users.noreply.github.com>
2024-07-23 13:06:08 +0000

    Arithmetic Operators

---
+++ Operators/Arithmetic-Operator/arithmOpr.c
@@ -0,0 +1,21 @@
+//Arithmetic Operator Example
+
+#include<stdio.h>
+int main()
+{
+    int a = 10 + 20;
+    int b = 50 - 10;
+    int c = 5 * 5;
+    float d = 5/2.0;
+    float e = 5%2;
+    printf("\na = %d", a);
+    printf("\nb = %d", b);
+    printf("\nc = %d", c);
+    printf("\nd = %f", d);
+    printf("\ne = %f", e);
+    printf("\n");
+    return 0;
+}
+
+//Arithmetic Operators are also called as Mathematical operators
+//Arithmetic Operators are +, -, *, /, %
