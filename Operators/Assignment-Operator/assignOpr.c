0622b17ba49ac07a6880568bece83c56039abeac
Yash Walke <142409974+Yash-6503@users.noreply.github.com>
2024-07-23 12:47:33 +0000

    Assignment Operator

---
+++ Operators/Assignment-Operator/assignOpr.c
@@ -0,0 +1,15 @@
+//Assignment Operator Examples
+
+#include<stdio.h>
+int main()
+{
+    int a = 10;         // = is the assignment operator
+    float b = 2.5;
+    char c = 'A';
+    printf("\na = %d", a);
+    printf("\nb = %f", b);
+    printf("\nc = %c", c);
+    return 0;
+}
+
+//Assignment Operator '=' is used to assigning the value to the variable
