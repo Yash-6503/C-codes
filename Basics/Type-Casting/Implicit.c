c1e3f3735b864c88482ee870929f5ebcf535aa97
Yash Walke <142409974+Yash-6503@users.noreply.github.com>
2024-07-23 12:28:29 +0000

    Implicit Type Casting

---
+++ Basics/Type-Casting/Implicit.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a = 2.5;    //Implicit Type Casting
+    float b = 5;    //Implicit Type Casting
+    printf("a = %d", a);
+    printf("\nb = %f", b);
+    return 0;
+}
