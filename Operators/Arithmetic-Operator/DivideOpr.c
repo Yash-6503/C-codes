cc97a8bc3502566d2578768e1b2ec3ea68048278
Yash Walke <142409974+Yash-6503@users.noreply.github.com>
2024-07-23 13:34:58 +0000

    Arithmetic Division[/] Operator example

---
+++ Operators/Arithmetic-Operator/DivideOpr.c
@@ -0,0 +1,16 @@
+//Division Operator example
+
+#include<stdio.h>
+int main()
+{
+    int a = 5/2;
+    float b = 5/2.0;
+    printf("a = %d", a);
+    printf("\nb = %f", b);
+    printf("\n%d", 123/10);
+    printf("\n%d", 73/10);
+    return 0;
+}
+
+//In Division Operator [/] 
+// Int/int = int. if anyone operand or both are floats then result is float.
