71380d6be740c3d6c9a35b38000da24f63a1b978
Yash Walke <142409974+Yash-6503@users.noreply.github.com>
2024-07-25 18:14:55 +0000

    Logical Operators example

---
+++ Operators/Logical-Operator/ex2.c
@@ -0,0 +1,15 @@
+//Logical Operators Example
+
+#include<stdio.h>
+int main()
+{
+    printf("") && printf("Hi ");
+    printf("Hello ") || printf("Thank you ");
+    printf("See you ") && printf("Have a nice day");
+    return 0;
+}
+
+/*
+output
+Hello See you Have a nice day
+*/
