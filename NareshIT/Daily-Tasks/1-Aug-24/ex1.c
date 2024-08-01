e79ec9d8f280caa3a0bf0b4a6e788a68ef51cc56
Yash Walke <142409974+Yash-6503@users.noreply.github.com>
2024-08-01 13:58:02 +0000

    Daily task

---
+++ NareshIT/Daily-Tasks/1-Aug-24/ex1.c
@@ -0,0 +1,13 @@
+// Write a C program to swap two numbers without using a third variable.
+
+#include<stdio.h>
+void main()
+{
+    int a, b;
+    printf("\nEnter a value: ");
+    scanf("%d", &a);
+    printf("\nEnter b value: ");
+    scanf("%d", &b);
+    printf("\na = %d", b%10);
+    printf("\nb = %d", a%10);
+}
