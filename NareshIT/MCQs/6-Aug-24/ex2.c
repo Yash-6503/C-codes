b5bf393cac0341d95e88c19d37904ff5ff3184e2
Yash Walke <142409974+Yash-6503@users.noreply.github.com>
2024-08-06 13:24:42 +0000

    MCQ q2 find output

---
+++ NareshIT/MCQs/6-Aug-24/ex2.c
@@ -0,0 +1,21 @@
+// What will be the output of the C program?
+
+#include<stdio.h>
+int main() 
+{ 
+    printf("%d",3 * 2-); 
+    return 0;
+}
+
+/* output
+Compilation Error
+*/
+
+/* output explanation
+
+Compilation Error occurs because of unexpected token '-'
+
+error: expected expression before ‘)’ token
+    6 |     printf("%d",3 * 2-);
+      |                       ^
+*/
