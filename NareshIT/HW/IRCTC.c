513a157887c7004f3a09e59515c71e4eb76bacdc
Yash Walke <142409974+Yash-6503@users.noreply.github.com>
2024-08-15 12:48:23 +0000

    IRCTC Login page Project

--- NareshIT/HW/IRCTC.c
+++ NareshIT/HW/IRCTC.c
@@ -12,48 +12,73 @@ void main()
     char user[10];
     char pass[10];
     char captcha[10];
+    int count = 1;
     
     login:
-    printf("\nEnter a username: ");
+    printf("\n\nEnter a username: ");
     scanf("%s", user);
     printf("\nEnter a password: ");
     scanf("%s", pass);
     printf("\nEnter Captcha eLrYHW: ");
     scanf("%s", captcha);
 
-    if(strcmp(user, username) == 0 && strcmp(pass, password) == 0 && strcmp(captcha, Captcha) == 0)
+    if(count < 3)
     {
-        printf("\nWelcome to IRCTC.");
-    }
-    else
-    {
-        if(!strcmp(user, username) == 0 && !strcmp(pass, password) == 0 && !strcmp(captcha, Captcha) == 0)
+        if(strcmp(user, username) == 0 && strcmp(pass, password) == 0 && strcmp(captcha, Captcha) == 0)
         {
-            printf("\nInvalid Credantials");
+            printf("\nWelcome to IRCTC.");
         }
-        else if(!strcmp(user, username) == 0 && strcmp(captcha, Captcha)==0)
+        else
         {
-            printf("\nInvalid username");
-        }
-        else if(!strcmp(pass, password) == 0 && strcmp(captcha, Captcha) == 0)
-        {
-            printf("\nInvalid password");
-        }
-        else if(!strcmp(user, username)==0 && !strcmp(captcha, Captcha)==0)
-        {
-            printf("\nInvalid Username and Captcha");
-        }
-        else if(!strcmp(pass, password)==0 && !strcmp(captcha, Captcha)==0)
-        {
-            printf("\nInvalid Password and Captcha");
-        }
-        else{
-            printf("\nInvalid Captcha");
-        }
-        
-    }    
-}
+            if(!strcmp(user, username) == 0 && !strcmp(pass, password) == 0 && !strcmp(captcha, Captcha) == 0)
+            {
+                printf("\nInvalid Credantials");
+                count++;
+                goto login;
+            }
+            else if(!strcmp(user, username) == 0 && strcmp(pass, password) == 0 && strcmp(captcha, Captcha)==0)
+            {
+                printf("\nInvalid username");
+                count++;
+                goto login;
+            }
+            else if(!strcmp(pass, password) == 0 && strcmp(user, username) == 0 && strcmp(captcha, Captcha) == 0)
+            {
+                printf("\nInvalid password");
+                count++;
+                goto login;
+            }
+            else if(!strcmp(user, username) == 0 && !strcmp(pass, password) == 0 && strcmp(captcha, Captcha) == 0)
+            {
+                printf("\nInvalid Username and Password");
+                count++;
+                goto login;
+            }
+            else if(!strcmp(user, username)==0 && !strcmp(captcha, Captcha)==0)
+            {
+                printf("\nInvalid Username and Captcha");
+                count++;
+                goto login;
+            }
+            else if(!strcmp(pass, password)==0 && !strcmp(captcha, Captcha)==0)
+            {
+                printf("\nInvalid Password and Captcha");
+                count++;
+                goto login;
+            }
+            else{
+                printf("\nInvalid Captcha");
+                count++;
+                goto login;
+            }
 
+               
+        }
+    }
+    else{
+        printf("\nYou have Finished 3 Attempts\nSo please Login After 5 minutes.");
+    }
+}
 
 /*
 Enter a username: aaa
@@ -67,6 +92,13 @@ Enter a username: Admin
 Enter a password: aa
 Enter Captcha eLrYHW: aaa
 Invalid Password and Captcha
+*/
+
+/*
+Enter a username: Admin@123
+Enter a password: Admin
+Enter Captcha eLrYHW: eLrYHW
+Invalid Username and Password
 */
 
 /*
@@ -102,4 +134,26 @@ Enter a username: Admin
 Enter a password: Admin@123
 Enter Captcha eLrYHW: eLrYHW
 Welcome to IRCTC.
+*/
+
+
+/*
+Enter a username: a
+Enter a password: a
+Enter Captcha eLrYHW: a
+
+Invalid Credantials
+
+Enter a username: b
+Enter a password: b
+Enter Captcha eLrYHW: b
+
+Invalid Credantials
+
+Enter a username: c
+Enter a password: c
+Enter Captcha eLrYHW: c
+
+You have Finished 3 Attempts
+So please Login After 5 minutes.
 */
