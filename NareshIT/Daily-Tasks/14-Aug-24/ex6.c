10f0925ad6afe0fcd7ca0a39733049ed8f114772
Yash Walke <142409974+Yash-6503@users.noreply.github.com>
2024-08-15 13:40:20 +0000

    Take Hours and minutes & Display it in AM or PM format

---
+++ NareshIT/Daily-Tasks/14-Aug-24/ex6.c
@@ -0,0 +1,42 @@
+/*
+6.
+Write a C program by taking one integer value from the user, if the 
+given number's last digit is smaller than 5 then print the square of
+that number else print that same number.
+
+Case-1:
+Enter a number : 12
+
+Output is :
+144
+
+Case 2 :
+Enter a number : 17
+
+Output is :
+17
+*/
+
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("\nEnter a number: ");
+    scanf("%d",&n);
+    if(n%10 < 5)
+    {
+        printf("\nSquare of %d is %d", n, n*n);
+    }
+    else
+    {
+        printf("\nThe number is %d", n);
+    }
+}
+
+/*
+Enter a number: 12
+Square of 12 is 144
+
+Enter a number: 17
+The number is 17
+*/
