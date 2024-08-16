70bf5933d946879d239fc0d6e48211de311f509a
Yash Walke <142409974+Yash-6503@users.noreply.github.com>
2024-08-16 18:07:38 +0000

    Take month as input & retun number of days in month

---
+++ NareshIT/Daily-Tasks/16-Aug-24/ex3.c
@@ -0,0 +1,52 @@
+/*
+3.
+Write a C program to enter month number between(1-12) and print 
+number of days in month using if else. How to print number of days 
+in a given month using if else in C programming. Logic to find 
+number of days in a month in C program. 
+
+Input as :
+Enter month number (1-12): 4
+
+Output as :
+Number of days: 30
+*/
+
+#include<stdio.h>
+void main()
+{
+    int month;
+    printf("\nEnter Month: ");
+    scanf("%d",&month);
+    if(month <= 12)
+    {
+        if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+        {
+            printf("\nNumber of days: 31");
+        }
+        else if(month == 2)
+        {
+            printf("\nNumber of days: 28");
+        }
+        else
+        {
+            printf("\nNumber of days: 30");
+        }
+    }
+    else
+    {
+        printf("\nInvalid Month\nPlease enter month between 1-12\n");
+    }
+}
+
+
+/*
+Enter Month: 4
+Number of days: 30
+
+Enter Month: 2
+Number of days: 28
+
+Enter Month: 12
+Number of days: 31
+*/
