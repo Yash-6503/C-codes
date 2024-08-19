6deb86c01f8d87f9ae270364001fa5399f849782
Yash Walke <142409974+Yash-6503@users.noreply.github.com>
2024-08-19 13:53:33 +0000

    Hotel menu driven program here we calculated bill according to items

--- NareshIT/Daily-Tasks/19-Aug-24/ex1.c
+++ NareshIT/Daily-Tasks/19-Aug-24/ex1.c
@@ -67,7 +67,154 @@ Attendant: "Thank you for visiting! We look forward to serving you again soon!"
 */
 
 #include<stdio.h>
+#include<stdlib.h>
 void main()
 {
+    int choice, qty;
+    float bill=0, bill_paid;
+    char confirm;
+
+    menu:
+    printf("\nWelcome to our Tea Stall Counter! \nOur menu:");
+    printf("\n1. Tea Rs.10");
+    printf("\n2. Coffee Rs.20");
+    printf("\n3. Cold Coffee Rs.50");
+    printf("\n4. Exit");
+    printf("\nEnter your Choice: ");
+    scanf("%d", &choice);
     
-}
+    switch(choice)
+    {
+        case 1: 
+            printf("\nEnter How many cups of refreshing tea?: ");
+            scanf("%d", &qty);
+            bill = bill + qty * 10;
+            break;
+        
+        case 2:
+            printf("\nEnter How many cups of aromatic coffee?: ");
+            scanf("%d", &qty);
+            bill = bill + qty * 20;
+            break;
+
+        case 3:
+            printf("\nEnter How many cups of chilled cold coffee?: ");
+            scanf("%d", &qty);
+            bill = bill + qty * 50;
+            break;
+
+        case 4:
+            exit(0);
+            
+        default:
+            printf("\nInvalid Choice\n");
+            goto menu;
+
+    }
+
+    printf("\nTotal for %d cup(s): Rs. %.2f.\n", qty, bill);
+
+    pay:
+    printf("\nEnter Amount to pay Bill: ");
+    scanf("%f", &bill_paid);
+    
+    if(bill_paid > bill)
+    {
+        printf("\nYour Change is %.2f", bill_paid - bill);
+        printf("\nBill Paid\n");
+        bill = 0;
+    }
+    else if(bill_paid == bill)
+    {
+        printf("\nBill Paid");
+        bill = 0;
+    }
+    else
+    {
+        printf("\nAmount paid is less Rs.%.2f than Bill Amount Rs.%.2f\n", bill_paid, bill);
+        bill = 0;
+        goto pay;
+    }
+
+    printf("\nExplore more or finalize? (Type 'Y' for Yes or 'N' for No): ");
+    scanf(" %c", &confirm);
+
+    if(confirm == 'y' || confirm == 'Y')
+    {
+        printf("\nCertainly, let's explore.\n");
+        goto menu;
+    }
+    else
+    {
+        printf("\nThank you for visiting!\n We look forward to serving you again soon!\n");
+    }
+}   
+
+
+/*
+Welcome to our Tea Stall Counter! 
+Our menu:
+1. Tea Rs.10
+2. Coffee Rs.20
+3. Cold Coffee Rs.50
+4. Exit
+Enter your Choice: 1
+
+Enter How many cups of refreshing tea?: 1
+
+Total for 1 cup(s): Rs. 10.00.
+
+Enter Amount to pay Bill: 9 
+
+Amount paid is less Rs.9.00 than Bill Amount Rs.10.00
+
+Enter Amount to pay Bill: 10
+
+Your Change is 10.00
+Bill Paid
+
+Explore more or finalize? (Type 'Y' for Yes or 'N' for No): y
+
+Certainly, let's explore.
+
+Welcome to our Tea Stall Counter! 
+Our menu:
+1. Tea Rs.10
+2. Coffee Rs.20
+3. Cold Coffee Rs.50
+4. Exit
+Enter your Choice: 3
+
+Enter How many cups of chilled cold coffee?: 2
+
+Total for 2 cup(s): Rs. 100.00.
+
+Enter Amount to pay Bill: 100
+
+Bill Paid
+Explore more or finalize? (Type 'Y' for Yes or 'N' for No): y
+
+Certainly, let's explore.
+
+Welcome to our Tea Stall Counter! 
+Our menu:
+1. Tea Rs.10
+2. Coffee Rs.20
+3. Cold Coffee Rs.50
+4. Exit
+Enter your Choice: 2
+
+Enter How many cups of aromatic coffee?: 3
+
+Total for 3 cup(s): Rs. 60.00.
+
+Enter Amount to pay Bill: 100
+
+Your Change is 40.00
+Bill Paid
+
+Explore more or finalize? (Type 'Y' for Yes or 'N' for No): n
+
+Thank you for visiting!
+ We look forward to serving you again soon!
+*/
