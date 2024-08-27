a6cd8bef369f24ab9d63d898794683dc8ca00f23
Yash Walke <142409974+Yash-6503@users.noreply.github.com>
2024-08-27 14:00:24 +0000

    Program to display the sum of n natural numbers using loops

---
+++ NareshIT/Daily-Tasks/27-Aug-24/ex3.c
@@ -0,0 +1,48 @@
+/*
+3.
+Write a program in C to display n terms of natural numbers and their sum. 
+Sample Input
+Test Data : 7
+Sample Output
+The first 7 natural number is :
+
+1 2 3 4 5 6 7
+
+The Sum of Natural Numbers upto 7 terms : 28
+*/
+
+#include<stdio.h>
+void main()
+{
+    int n, i=1, sum=0, m;
+    printf("\nEnter number: ");
+    scanf("%d", &n);
+    printf("\nThe first %d natural number is: ", n);
+    for(i=1; i<=n; i++)
+    {
+        printf("%d ", i);
+        sum += i;
+    }
+    m=n;
+    // while(n!=0)
+    // {
+    //     sum+=i;
+    //     printf("%d ", i++);
+    //     n--;
+    // }
+    printf("\nThe sum of Natural Numbers upto %d terms: %d", m, sum);
+}
+
+/*
+Enter number: 7
+
+The first 7 natural number is: 1 2 3 4 5 6 7 
+The sum of Natural Numbers upto 7 terms: 28
+*/
+
+/*
+Enter number: 100
+
+The first 100 natural number is: 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100 
+The sum of Natural Numbers upto 100 terms: 5050
+*/
