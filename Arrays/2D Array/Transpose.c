608766c33e1d6e85b78f6c975ca700a386e958b1
Yash Walke <142409974+Yash-6503@users.noreply.github.com>
2024-09-10 18:31:36 +0000

    Program to find Transpose of n*n matrix

---
+++ Arrays/2D Array/Transpose.c
@@ -0,0 +1,43 @@
+/*
+Write a program to find Transpose of n*n matrix
+*/
+
+#include<stdio.h>
+void main()
+{
+    int a[10][10], nr, nc, sum=0, r, c;
+    printf("\nEnter number of rows & columns: ");
+    scanf("%d %d", &nr, &nc);
+    printf("\nEnter %d elements: ", nr*nc);
+    for(r=0; r<nr; r++)
+    {
+        for(c=0; c<nc; c++)
+        {
+            scanf("%d", &a[r][c]);
+        }
+    }
+
+    printf("\nTranspose Matrix is: \n");
+    for(c=0; c<nc; c++)
+    {
+        for(r=0; r<nr; r++)
+        {
+            printf("%3d", a[r][c]);
+        }
+        printf("\n");
+    }
+}
+
+/*
+Enter number of rows & columns: 3 3
+
+Enter 9 elements: 
+1 2 3
+4 5 6
+7 8 9
+
+Transpose Matrix is: 
+  1  4  7
+  2  5  8
+  3  6  9
+*/
